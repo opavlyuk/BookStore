294c0c95fe7b92dbd3852bc647402638a3e908a5
opavliuk <opavliuk@llnw.com>
2020-09-16 16:03:56 +0300

    Add contoller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,27 +11,12 @@ namespace BookStore.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        BookContext db = new BookContext();
+        public ActionResult Index()
         {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
+            IEnumerable<Book> books = db.Books;
+            ViewBag.Books = books;
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
